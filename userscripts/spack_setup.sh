1d5df50ecadc1ec31746361064c1c2b13ca52d3c
dpath2o <dpath2o@mac.com>
2026-02-13 17:10:01 +1100

    2026-02-13 17:10:01: Run 0

--- userscripts/spack_setup.sh
+++ userscripts/spack_setup.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source /g/data/gv90/da1339/spack/0.22/spack/share/spack/setup-env.sh
 spack env activate -p /g/data/gv90/da1339/spack/0.22/environments/om3_cice6_dev
-spack load access-om3
+#spack load access-om3
 
 echo "=== runtime sanity ==="
 echo "which access-om3-MOM6-CICE6: $(command -v access-om3-MOM6-CICE6 || true)"
